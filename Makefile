6a65c7557de632fe85338bbe51fcb06e02857724
Kyle Andrews <codingwithkyle@gmail.com>
2025-01-05 13:35:23 -0500

    Fixed native

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ main: main.c
 	clang -Wall -Wextra -Wswitch-enum -Os -fno-builtin --target=wasm32 -I./raylib/include --no-standard-libraries -Wl,--no-entry -Wl,--allow-undefined -Wl,--export=render_init -Wl,--export=next_frame -Wl,--export=render_end -o main.wasm main.c -DPLATFORM_WEB
 
 native: main.c
-	clang -Wall -Wextra -Wswitch-enum -Os -fno-builtin -I./raylib/include --no-standard-libraries -o main main.c
+	clang -I./raylib/include -o main main.c -L./raylib/lib/ -l:libraylib.a -lm -DPLATFORM_NATIVE
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <raylib.h>
+#include <raymath.h>
+
+#define BALL_RADIUS 100.0
+
+static Vector2 ball_position = {0};
+static Vector2 ball_velocity = {100, 100};
 
 void next_frame()
 {
     BeginDrawing();
-    ClearBackground(RED);
+        ClearBackground((Color){18,18,18,255});
+        ball_position = Vector2Add(ball_position, Vector2Scale(ball_velocity, GetFrameTime()));
+        DrawCircleV(ball_position, BALL_RADIUS, RED);
     EndDrawing();
 }
 
@@ -11,6 +19,10 @@ void render_init()
 {
     InitWindow(800, 600, "Hello from WASM");
     SetTargetFPS(60);
+    int w = GetScreenWidth();
+    int h = GetScreenHeight();
+    ball_position.x = w * .5;
+    ball_position.y = h * .5;
 }
 
 void render_close()
@@ -18,7 +30,7 @@ void render_close()
     CloseWindow();
 }
 
-#ifdef PLATFORM_WEB
+#ifdef PLATFORM_NATIVE
 int main()
 {
     render_init();
