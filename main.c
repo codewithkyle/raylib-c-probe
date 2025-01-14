a4e6b0191160dd5b0a25c19a2ff5e51ba93d0f89
codewithkyle <codewithkyle@users.noreply.github.com>
2025-01-14 17:41:05 +0000

    deploy: c903f0740154e1634291850b6ee9435769f8f76e

---
+++ main.c
@@ -0,0 +1,72 @@
+#include <raylib.h>
+float floorf(float);
+float fabsf(float);
+double fabs(double);
+float fmaxf(float, float);
+float fminf(float, float);
+float sqrtf(float);
+float atan2f(float, float);
+float cosf(float);
+float sinf(float);
+double tan(double);
+float acosf(float);
+float asinf(float);
+#include <raymath.h>
+
+#define BALL_RADIUS 100.0
+#define GRAVITY 1000.0
+
+static Vector2 ball_position = {0};
+static Vector2 ball_velocity = {200, 200};
+
+void next_frame()
+{
+    BeginDrawing();
+        ClearBackground((Color){18,18,18,255});
+        float dt = GetFrameTime();
+        Vector2 new_ball_position = Vector2Add(ball_position, Vector2Scale(ball_velocity, dt));
+        ball_velocity.y += GRAVITY*dt;
+        ball_velocity.y -= ball_velocity.y * 0.01f;
+        if (new_ball_position.x - BALL_RADIUS < 0.0 || new_ball_position.x + BALL_RADIUS >= GetScreenWidth())
+        {
+            ball_velocity.x *= -1.0f;
+        } else {
+            ball_position.x = new_ball_position.x;
+        }
+        if (new_ball_position.y - BALL_RADIUS < 0.0 || new_ball_position.y + BALL_RADIUS >= GetScreenHeight())
+        {
+            ball_velocity.y *= -1.0f;
+        } else {
+            ball_position.y = new_ball_position.y;
+        }
+        DrawCircleV(ball_position, BALL_RADIUS, RED);
+    EndDrawing();
+}
+
+void render_init()
+{
+    InitWindow(800, 600, "Hello from WASM");
+    SetTargetFPS(60);
+    int w = GetScreenWidth();
+    int h = GetScreenHeight();
+    ball_position.x = w * .5;
+    ball_position.y = h * .5;
+}
+
+void render_close()
+{
+    CloseWindow();
+}
+
+#ifdef PLATFORM_NATIVE
+int main()
+{
+    render_init();
+    while (!WindowShouldClose())
+    {
+        next_frame();
+    }
+    render_close();
+    return 0;
+}
+#endif
---
+++ main.js
@@ -0,0 +1,130 @@
+/** @type {number} */
+let previous = undefined;
+let w = undefined;
+
+/** @type {CanvasRenderingContext2D} */
+let ctx = undefined;
+
+/** @type {number} */
+let dt = undefined;
+
+let doUpdate = true;
+let skipNextUpdate = false;
+
+document.addEventListener("visibilitychange", ()=>{
+    doUpdate = !document.hidden;
+    if (!doUpdate) skipNextUpdate = true;
+});
+
+WebAssembly.instantiateStreaming(fetch("main.wasm"), {
+    env: make_env({
+        InitWindow: (width, height, text_ptr) => {
+            console.log("InitWindow", width, height, text_ptr);
+            ctx.canvas.height = height;
+            ctx.canvas.width = width;
+            const buffer = w.instance.exports.memory.buffer;
+            const text = cstr_by_ptr(buffer, text_ptr);
+            document.title = text;
+        },
+        SetTargetFPS: (fps) => {
+            console.log(`We want to run at ${fps} FPS, but on the Web we're just going to ignore it.`);
+        },
+        BeginDrawing: ()=>{},
+        EndDrawing: ()=>{},
+        ClearBackground: (color_ptr)=>{
+            const buffer = w.instance.exports.memory.buffer;
+            const hex = color_hex_unpacked(...new Uint8Array(buffer, color_ptr, 4));
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.fillStyle = hex;
+            ctx.fill();
+        },
+        GetScreenWidth: () => ctx.canvas.width,
+        GetScreenHeight: () => ctx.canvas.height,
+        GetFrameTime: () => dt,
+        DrawCircleV: (center_ptr, radius, color_ptr) => {
+            const buffer = w.instance.exports.memory.buffer;
+            const [x, y] = new Float32Array(buffer, center_ptr, 2);
+            const hex = color_hex_unpacked(...new Uint8Array(buffer, color_ptr, 4));
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 2*Math.PI);
+            ctx.fillStyle = hex;
+            ctx.closePath();
+            ctx.fill();
+        },
+    }),
+}).then((wasm) => {
+    console.log("WASM instantiated", wasm);
+    w = wasm;
+    /** @type {HTMLCanvasElement} */
+    const canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    w.instance.exports.render_init();
+    window.requestAnimationFrame(first);
+}).catch((e) => {
+    console.error("Failed to instantiate WASM", e);
+});
+
+function first(timestamp) {
+    previous = timestamp;
+    window.requestAnimationFrame(next);
+}
+function next(timestamp) {
+    dt = (timestamp - previous)*0.001;
+    previous = timestamp;
+    if (doUpdate){
+        if (!skipNextUpdate) {
+            w.instance.exports.next_frame();
+        } else {
+            skipNextUpdate = false;
+        }
+    }
+    window.requestAnimationFrame(next);
+}
+
+function make_env(...envs) {
+    return new Proxy(envs, {
+        get(target, prop, receiver) {
+            for (let env of envs) {
+                if (env.hasOwnProperty(prop)) {
+                    return env[prop];
+                }
+            }
+            return (...args) => {
+                console.error("NOT IMPLEMENTED: " + prop, args);
+            }
+        }
+    });
+}
+
+function cstrlen(mem, ptr) {
+    let len = 0;
+    while (mem[ptr] != 0) {
+        len++;
+        ptr++;
+    }
+    return len;
+}
+
+function cstr_by_ptr(mem_buffer, ptr) {
+    const mem = new Uint8Array(mem_buffer);
+    const len = cstrlen(mem, ptr);
+    const bytes = new Uint8Array(mem_buffer, ptr, len);
+    return new TextDecoder().decode(bytes);
+}
+
+function color_hex_unpacked(r, g, b, a) {
+    r = r.toString(16).padStart(2, "0");
+    g = g.toString(16).padStart(2, "0");
+    b = b.toString(16).padStart(2, "0");
+    a = a.toString(16).padStart(2, "0");
+    return `#${r}${g}${b}${a}`;
+}
+
+function color_hex(color) {
+    const r = ((color>>(0*8))&0xFF).toString(16).padStart(2, "0");
+    const g = ((color>>(1*8))&0xFF).toString(16).padStart(2, "0");
+    const b = ((color>>(2*8))&0xFF).toString(16).padStart(2, "0");
+    const a = ((color>>(3*8))&0xFF).toString(16).padStart(2, "0");
+    return `#${r}${g}${b}${a}`;
+}
